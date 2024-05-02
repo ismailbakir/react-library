eccb2a7551d87a75b156ee4fbad1fe410eba9883
ismailbakir <bakirismail40@gmail.com>
2024-05-02 23:17:12 +0300

    Carousel sınıfında kitapların useState ile db den getirrilmesi sağlandı.

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,7 +1,69 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../../models/BookModel";
 import { ReturnBook } from "./ReturnBook";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export const Carousel = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseUrl = "http://localhost:8080/api/books";
+
+      const url = `${baseUrl}?page=0size9`;
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Books servisinden istenen alınamadı!");
+      }
+
+      const jsonResponse = await response.json();
+
+      const responseData = jsonResponse._embedded.books;
+
+      const loadedBooks: BookModel[] = [];
+
+      for (const key in responseData) {
+        loadedBooks.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img,
+        });
+      }
+
+      setBooks(loadedBooks);
+      setIsLoading(false);
+    };
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="container m-5">
+        <p>Yükleniyor..</p>
+      </div>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container mt-5" style={{ height: 550 }}>
       <div className="homepage-carousel-title">
@@ -16,16 +78,23 @@ export const Carousel = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(0, 3).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(3, 6).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="row d-flex justify-content-center align-items-center">
+              {books.slice(6, 9).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
         </div>
@@ -57,7 +126,7 @@ export const Carousel = () => {
       {/* Mobile */}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center align-items-center">
-          <ReturnBook />
+          <ReturnBook book={books[7]} key={books[7].id} />
         </div>
       </div>
       <div className="homepage-carousel-title mt-3">
--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import BookModel from "../../../models/BookModel";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
       <div className="text-center">
-        <img
-          src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
-          width="151"
-          height="233"
-          alt="book"
-        />
-        <h6 className="mt-2">Kitap</h6>
-        <p>Luv2Code</p>
+        {props.book.img ? (
+          <img src={props.book.img} width="151" height="233" alt="book" />
+        ) : (
+          <img
+            src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
+            width="151"
+            height="233"
+            alt="book"
+          />
+        )}
+
+        <h6 className="mt-2">{props.book.title}</h6>
+        <p>{props.book.author}</p>
         <a className="btn main-color text-white" href="#">
           Rezerv
         </a>
