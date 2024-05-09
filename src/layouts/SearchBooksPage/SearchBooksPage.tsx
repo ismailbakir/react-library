0db8689685e2aef2332c22402e4dd34c4746a4b6
İsmail Bakır <bakirismail40@gmail.com>
2024-05-09 13:43:49 +0300

    Update SearchBooksPage.tsx
    Arama bölümünde bir kitap bulunamazsa görünüm özelleştirildi.

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -107,9 +107,14 @@ export const SearchBooksPage = () => {
                   className="form-control me-2"
                   placeholder="Search"
                   aria-labelledby="Search"
-                  onChange={e =>setSearch(e.target.value)}
+                  onChange={(e) => setSearch(e.target.value)}
                 />
-                <button className="btn btn-outline-success" onClick={() => searchHandleChange()}>Search</button>
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => searchHandleChange()}
+                >
+                  Ara
+                </button>
               </div>
             </div>
             <div className="col-4">
@@ -156,16 +161,31 @@ export const SearchBooksPage = () => {
               </div>
             </div>
           </div>
-          <div className="mt-3">
-            <h5>Sonuç sayısı : ({totalAmountOfBooks})</h5>
-          </div>
-          <p>
-            {indexOfFirstBook + 1} ile {lastItem} arasında / Toplam{" "}
-            {totalAmountOfBooks} öğe{" "}
-          </p>
-          {books.map((book) => (
-            <SearchBook book={book} key={book.id} />
-          ))}
+          {totalAmountOfBooks > 0 ? (
+            <>
+              <div className="mt-3">
+                <h5>Sonuç sayısı : ({totalAmountOfBooks})</h5>
+              </div>
+              <p>
+                {indexOfFirstBook + 1} ile {lastItem} arasında / Toplam{" "}
+                {totalAmountOfBooks} öğe{" "}
+              </p>
+              {books.map((book) => (
+                <SearchBook book={book} key={book.id} />
+              ))}
+            </>
+          ) : (
+            <div className="m-5">
+              <h3> Aradığını bulamıyor musun?</h3>
+              <a
+                type='button'
+                className='btn main-color btn-md px-4 me-md-2 fw-bold text-white'
+                href="#"
+              >
+                Kütüphane Servisi
+              </a>
+            </div>
+          )}
 
           {totalPages > 1 && (
             <Pagination
